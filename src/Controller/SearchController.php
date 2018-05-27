6ea4a4c6fbde6fc59167d83646cfe50769bb0302
Florian Backmeier <info@florianbackmeier.de>
2018-05-27 23:19:38 +0200

    Search for shared items and bug in textType

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Security\RSAService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,6 +27,26 @@ class SearchController extends AbstractController
                 }
             }
         }
-        return $this->render('search.html.twig', array_merge($this->defaultModel(), array('db' => $result)));
+        return $this->render('search.html.twig', array_merge($this->defaultModel(), array('db' => $result, 'searchKey' => $search)));
+    }
+
+    /**
+     * @Route("/search_shared", name="search_shared")
+     * @Security("has_role('ROLE_USER')")
+     */
+    public function searchSharedAction(Request $request, RSAService $RSAService)
+    {
+        $search = html_entity_decode($request->request->get('search'));
+        $result = array();
+        if ( !empty($search) ) {
+            $token = $this->get('security.token_storage')->getToken();
+            $sharedItems = $RSAService->getSharedItems($token);
+            foreach ( $sharedItems as $item ) {
+                if (stristr($item->getName(), $search)) {
+                    $result[] = $item;
+                }
+            }
+        }
+        return $this->render('searchAjax.html.twig', array_merge($this->defaultModel(), array('db' => $result)));
     }
 }
